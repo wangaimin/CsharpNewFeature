178d439bd325f56dbdeb94216226b4ac0272baa5
gene.a.wang <448497795@qq.com>
2019-04-21 07:49:56 +0800

    添加项目文件。

---
+++ CsharpNew/Csharp_6.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpNew
+{
+    /*
+     
+    Expression-bodied 函数成员     
+         
+         
+    */
+    static class Csharp_6 
+    {
+
+        /// <summary>
+        /// Expression-bodied 函数成员     
+        /// </summary>
+        /// <param name="lastName"></param>
+        /// <param name="firstName"></param>
+        /// <returns></returns>
+        public static string GetFullName(string lastName, string firstName)
+        {
+            return $"{lastName} {firstName}";
+        }
+    }
+}
---
+++ CsharpNew/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Csharp_6.GetFullName("Gene", "WANG"));
+            Console.ReadKey();
+        }
+    }
+}
