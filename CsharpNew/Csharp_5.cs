74c946b7fdb1e00d2242214e91ef344db0ebefe4
Gene.A.Wang <448497795@qq.com>
2019-05-05 10:52:00 +0800

    c#5  async await

---
+++ CsharpNew/Csharp_5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpNew
+{
+   public static class Csharp_5
+    {
+
+        public static async void GetHtmlStr(string url) {
+            HttpClient httpClient = new HttpClient();
+            Console.WriteLine("GetStringAsync Begin");
+            string str = await httpClient.GetStringAsync(url);
+            Console.WriteLine("GetStringAsync End");
+            Console.WriteLine($"Result:{str}");
+        }
+    }
+}
--- CsharpNew/Program.cs
+++ CsharpNew/Program.cs
@@ -10,6 +10,9 @@ namespace CsharpNew
     {
         static void Main(string[] args)
         {
+            Csharp_5.GetHtmlStr(@"https://docs.microsoft.com/zh-cn/dotnet/csharp/async");
+            
+
             Console.WriteLine(Csharp_6.GetFullName("Gene", "WANG"));
             Console.ReadKey();
         }
